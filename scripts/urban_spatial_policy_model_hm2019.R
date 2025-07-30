4e7c4ece5c322cc8ca779bfbf4877af238feb71e
chris <donbarno@gmail.com>
2025-07-30 09:52:28 -0700

    updated with a table for the result of the policy experiment

--- scripts/urban_spatial_policy_model_hm2019.R
+++ scripts/urban_spatial_policy_model_hm2019.R
@@ -18,6 +18,9 @@ rate <- params[4]
 theta_param <- params[5]
 land_param = 1 - alpha_param - eta_param
 
+tfp_constant = 
+  (alpha_param^(1-eta_param))*((eta_param/rate)^eta_param)
+
 treated_msas = 
   c(
     5600,
@@ -57,6 +60,8 @@ hsieh_moretti_data <-
     populat_saiz = ifelse( is.na(populat_saiz) == T, st_avg_pop, populat_saiz ),
     tfp_term_64 =  ( (emp1964^land_param)*(wage_condit_1964^(1-eta_param)) ),
     tfp_term_09 =  ( (emp2009^land_param)*(wage_condit_2009^(1-eta_param)) ),
+    local_tfp_64 = emp1964*(wage_condit_1964^((1-eta_param)/land_param))/tfp_constant,
+    local_tfp_09 = emp2009*(wage_condit_2009^((1-eta_param)/land_param))/tfp_constant,
     amenity_pm_64 = mean(wage_condit_2009)*(1 + (.33 * ( (HP64 - mean(HP64))/mean(HP64) )) - (.51 * ( (wage_condit_1964 - mean(wage_condit_1964))/mean(wage_condit_1964) )) ), 
     amenity_pm_09 = mean(wage_condit_2009)*(1 + (.33 * ( (HP09 - mean(HP09))/mean(HP09) )) - (.51 * ( (wage_condit_2009 - mean(wage_condit_2009))/mean(wage_condit_2009) )) ), 
     log_amenity_pm_09 = log(amenity_pm_09),
@@ -96,6 +101,29 @@ policy_df =
       (tfp_term_09^tfp_exponent_policy)*(q_raw_wage^amenity_exponent_policy),
     emp_policy = 
       (tfp_term_09/(wage_policy^(1-eta_param)) )^(1/land_param)
+  )
+
+# policy experiment output table
+policy_df |> 
+  mutate(
+    emp_diff = emp_policy - emp2009,
+    agg_emp_diff = sum(emp_diff),
+    rescale_factor = agg_emp_diff/sum(emp_policy), # hm do this rescaling move. is it valid?
+    emp_policy_prime = (1-rescale_factor)*emp_policy,
+    emp_share_prime = emp_policy_prime/sum(emp_policy_prime),
+    price_ratio_policy = sum(emp_share_prime*wage_policy),
+    agg_output_term_policy = sum( ((tfp_term_09/tfp_constant)*((price_ratio_policy/wage_policy)^(1-eta_param)))^(1/((1-eta_param)*(1+theta_param) - alpha_param)) ),
+    agg_output_policy = ((eta_param/rate)^(eta_param/(1-eta_param)))*(agg_output_term_policy^(((1-eta_param)*(1+theta_param) - alpha_param)/(1-eta_param))),
+    output_term_64 = sum( ((tfp_term_64/tfp_constant)*((price_avg_ratio_64/wage_condit_1964)^(1-eta_param)))^(1/((1-eta_param)*(1+theta_param) - alpha_param)) ),
+    output_term_09 = sum( ((tfp_term_09/tfp_constant)*((price_avg_ratio_09/wage_condit_2009)^(1-eta_param)))^(1/((1-eta_param)*(1+theta_param) - alpha_param)) ),
+    output_64 = ((eta_param/rate)^(eta_param/(1-eta_param)))*(output_term_64^(((1-eta_param)*(1+theta_param) - alpha_param)/(1-eta_param))),
+    output_09 = ((eta_param/rate)^(eta_param/(1-eta_param)))*(output_term_09^(((1-eta_param)*(1+theta_param) - alpha_param)/(1-eta_param))),
+    output_diff = (agg_output_policy - output_64)/(output_09 - output_64) - 1,
+    welfare_diff = ((agg_output_policy/price_ratio_policy) - (output_64/price_avg_ratio_64))/((output_09/price_avg_ratio_09) - (output_64/price_avg_ratio_64)) - 1
+  ) |>
+  summarize(
+    output_pct_diff = mean(output_diff)*100,
+    welfare_pct_diff = mean(welfare_diff)*100
   )
 
 # verification
